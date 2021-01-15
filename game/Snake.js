fc3f9f39d7c76c68585de14dfde46836bf96bb22
Vladislav <lex3a@users.noreply.github.com>
2021-01-15 17:39:12 +0300

    Split Snake to module

---
+++ game/Snake.js
@@ -0,0 +1,194 @@
+import { getRandomColor } from "./utils.js";
+
+export default class Snake {
+  constructor({
+    name = "Default",
+    keys,
+    x = 0,
+    y = 0,
+    color = getRandomColor(),
+    scorePosX,
+    scorePosY,
+  }) {
+    this.keys = keys;
+    this.x = x;
+    this.y = y;
+    this.scorePosX = scorePosX;
+    this.scorePosY = scorePosY;
+    this.color = color;
+    this.direction = "bottom";
+    this.width = 10;
+    this.score = 0;
+    this.height = 10;
+    this.name = name;
+    this.fpsInterval = 5;
+    this.displayGameOver = true;
+    this.snake = [
+      { x: 200 + this.x, y: 50 },
+      { x: 190 + this.x, y: 50 },
+      { x: 180 + this.x, y: 50 },
+      { x: 170 + this.x, y: 50 },
+    ];
+  }
+
+  setContext(ctx) {
+    this.ctx = ctx;
+  }
+
+  setHeadImg(img) {
+    this.headImg = img;
+  }
+
+  logKey(e) {
+    switch (e.code) {
+      case this.keys[0]:
+        if (this.direction === "bottom") return;
+        this.direction = "top";
+        break;
+      case this.keys[1]:
+        if (this.direction === "top") return;
+        this.direction = "bottom";
+        break;
+      case this.keys[2]:
+        if (this.direction === "right") return;
+        this.direction = "left";
+        break;
+      case this.keys[3]:
+        if (this.direction === "left") return;
+        this.direction = "right";
+        break;
+      default:
+        console.log("No direction", e.code);
+        break;
+    }
+  }
+
+  drawSnake() {
+    let headImgPos = 0;
+    this.ctx.fillStyle = this.color;
+    for (let i = 0; i < this.snake.length; i++) {
+      this.ctx.fillRect(
+        this.snake[i].x,
+        this.snake[i].y,
+        this.width,
+        this.height
+      );
+    }
+    switch (this.direction) {
+      case "top":
+        headImgPos = 0;
+        break;
+      case "right":
+        headImgPos = 10;
+        break;
+      case "bottom":
+        headImgPos = 20;
+        break;
+      case "left":
+        headImgPos = 30;
+        break;
+    }
+    this.ctx.drawImage(
+      this.headImg,
+      headImgPos,
+      0,
+      10,
+      10,
+      this.snake[0].x + 3,
+      this.snake[0].y,
+      this.width,
+      this.height
+    );
+  }
+
+  move() {
+    const head = { x: this.snake[0].x, y: this.snake[0].y };
+    switch (this.direction) {
+      case "bottom":
+        head.y += 10;
+        break;
+      case "right":
+        head.x += 10;
+        break;
+      case "left":
+        head.x -= 10;
+        break;
+      case "top":
+        head.y -= 10;
+        break;
+    }
+    this.snake.unshift(head);
+    this.snake.pop();
+  }
+
+  isSnakeOutsideCanvas() {
+    if (this.snake[0].x >= this.ctx.canvas.width) {
+      this.snake[0].x = 0;
+    } else if (this.snake[0].x < 0) {
+      this.snake[0].x = this.ctx.canvas.width - 1;
+    } else if (this.snake[0].y >= this.ctx.canvas.height) {
+      this.snake[0].y = 0;
+    } else if (this.snake[0].y < 0) {
+      this.snake[0].y = this.ctx.canvas.height;
+    }
+  }
+
+  isGameOver() {
+    for (let i = 4; i < this.snake.length; i++) {
+      const hasCollided =
+        this.snake[i].x === this.snake[0].x &&
+        this.snake[i].y === this.snake[0].y;
+      if (hasCollided) return true;
+    }
+  }
+
+  isEntitiesCollide(entity) {
+    for (let i = 0; i < entity.snake.length; i++) {
+      if (
+        ((this.snake[0].x >= entity.snake[i].x &&
+          this.snake[0].x <= entity.snake[i].x + this.width) ||
+          (this.snake[0].x + this.width >= entity.snake[i].x &&
+            this.snake[0].x + this.width <= entity.snake[i].x + this.width)) &&
+        ((this.snake[0].y >= entity.snake[i].y &&
+          this.snake[0].y <= entity.snake[i].y + this.height) ||
+          (this.snake[0].y + this.height >= entity.snake[i].y &&
+            this.snake[0].y + this.height <= entity.snake[i].y + this.height))
+      )
+        return true;
+    }
+  }
+
+  drawScore() {
+    this.ctx.fillStyle = this.color;
+    this.ctx.font = "12px 'Press Start 2P'";
+    this.ctx.fillText(`Beer: ${this.score}`, this.scorePosX, this.scorePosY);
+  }
+
+  drawGameOver(toggle) {
+    if (toggle) {
+      this.ctx.font = "48px serif";
+      this.ctx.fillText(
+        `${this.name} died!`,
+        this.ctx.canvas.width / 2 - 96,
+        this.ctx.canvas.height / 2
+      );
+      this.ctx.fillText(
+        `Score: ${this.score}`,
+        this.ctx.canvas.width / 2 - 48,
+        this.ctx.canvas.height / 2 - 48
+      );
+    }
+  }
+
+  update() {
+    if (!this.isGameOver()) {
+      this.isSnakeOutsideCanvas();
+      this.move();
+    } else {
+      this.drawGameOver(this.displayGameOver);
+      setTimeout(() => {
+        this.displayGameOver = false;
+      }, 4500);
+    }
+  }
+}
--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const beer = new Image();
 beer.src = "./assets/beer.png";
 snakeHead.src = "./assets/head.png";
 
-import { getRandomColor } from "./game/utils.js";
 import Food from "./game/Food.js";
+import Snake from "./game/Snake.js";
 
 let playerOne = {
   name: "Player One",
@@ -47,186 +47,6 @@ let playerFour = {
   scorePosY: canvas.height - 20,
 };
 
-class Snake {
-  constructor({
-    name = "Default",
-    keys,
-    x = 0,
-    y = 0,
-    color = getRandomColor(),
-    scorePosX,
-    scorePosY,
-  }) {
-    this.keys = keys;
-    this.x = x;
-    this.y = y;
-    this.scorePosX = scorePosX;
-    this.scorePosY = scorePosY;
-    this.color = color;
-    this.direction = "bottom";
-    this.width = 10;
-    this.score = 0;
-    this.height = 10;
-    this.name = name;
-    this.fpsInterval = 5;
-    this.displayGameOver = true;
-    this.snake = [
-      { x: 200 + this.x, y: 50 },
-      { x: 190 + this.x, y: 50 },
-      { x: 180 + this.x, y: 50 },
-      { x: 170 + this.x, y: 50 },
-    ];
-  }
-
-  logKey(e) {
-    switch (e.code) {
-      case this.keys[0]:
-        if (this.direction === "bottom") return;
-        this.direction = "top";
-        break;
-      case this.keys[1]:
-        if (this.direction === "top") return;
-        this.direction = "bottom";
-        break;
-      case this.keys[2]:
-        if (this.direction === "right") return;
-        this.direction = "left";
-        break;
-      case this.keys[3]:
-        if (this.direction === "left") return;
-        this.direction = "right";
-        break;
-      default:
-        console.log("No direction", e.code);
-        break;
-    }
-  }
-
-  drawSnake() {
-    let headImgPos = 0;
-    ctx.fillStyle = this.color;
-    for (let i = 0; i < this.snake.length; i++) {
-      ctx.fillRect(this.snake[i].x, this.snake[i].y, this.width, this.height);
-    }
-    switch (this.direction) {
-      case "top":
-        headImgPos = 0;
-        break;
-      case "right":
-        headImgPos = 10;
-        break;
-      case "bottom":
-        headImgPos = 20;
-        break;
-      case "left":
-        headImgPos = 30;
-        break;
-    }
-    ctx.drawImage(
-      snakeHead,
-      headImgPos,
-      0,
-      10,
-      10,
-      this.snake[0].x + 3,
-      this.snake[0].y,
-      this.width,
-      this.height
-    );
-  }
-
-  move() {
-    const head = { x: this.snake[0].x, y: this.snake[0].y };
-    switch (this.direction) {
-      case "bottom":
-        head.y += 10;
-        break;
-      case "right":
-        head.x += 10;
-        break;
-      case "left":
-        head.x -= 10;
-        break;
-      case "top":
-        head.y -= 10;
-        break;
-    }
-    this.snake.unshift(head);
-    this.snake.pop();
-  }
-
-  isSnakeOutsideCanvas() {
-    if (this.snake[0].x >= canvas.width) {
-      this.snake[0].x = 0;
-    } else if (this.snake[0].x < 0) {
-      this.snake[0].x = canvas.width - 1;
-    } else if (this.snake[0].y >= canvas.height) {
-      this.snake[0].y = 0;
-    } else if (this.snake[0].y < 0) {
-      this.snake[0].y = canvas.height;
-    }
-  }
-
-  isGameOver() {
-    for (let i = 4; i < this.snake.length; i++) {
-      const hasCollided =
-        this.snake[i].x === this.snake[0].x &&
-        this.snake[i].y === this.snake[0].y;
-      if (hasCollided) return true;
-    }
-  }
-
-  isEntitiesCollide(entity) {
-    for (let i = 0; i < entity.snake.length; i++) {
-      if (
-        ((this.snake[0].x >= entity.snake[i].x &&
-          this.snake[0].x <= entity.snake[i].x + this.width) ||
-          (this.snake[0].x + this.width >= entity.snake[i].x &&
-            this.snake[0].x + this.width <= entity.snake[i].x + this.width)) &&
-        ((this.snake[0].y >= entity.snake[i].y &&
-          this.snake[0].y <= entity.snake[i].y + this.height) ||
-          (this.snake[0].y + this.height >= entity.snake[i].y &&
-            this.snake[0].y + this.height <= entity.snake[i].y + this.height))
-      )
-        return true;
-    }
-  }
-
-  drawScore() {
-    ctx.fillStyle = this.color;
-    ctx.font = "12px 'Press Start 2P'";
-    ctx.fillText(`Beer: ${this.score}`, this.scorePosX, this.scorePosY);
-  }
-
-  drawGameOver(toggle) {
-    if (toggle) {
-      ctx.font = "48px serif";
-      ctx.fillText(
-        `${this.name} died!`,
-        canvas.width / 2 - 96,
-        canvas.height / 2
-      );
-      ctx.fillText(
-        `Score: ${this.score}`,
-        canvas.width / 2 - 48,
-        canvas.height / 2 - 48
-      );
-    }
-  }
-
-  update() {
-    if (!this.isGameOver()) {
-      this.isSnakeOutsideCanvas();
-      this.move();
-    } else {
-      this.drawGameOver(this.displayGameOver);
-      setTimeout(() => {
-        this.displayGameOver = false;
-      }, 4500);
-    }
-  }
-}
-
 let snakes = [
   new Snake(playerOne),
   new Snake(playerTwo),
@@ -236,6 +56,8 @@ let snakes = [
 let foods = [new Food(canvas, beer), new Food(canvas, beer)];
 
 for (let snake of snakes) {
+  snake.setContext(ctx);
+  snake.setHeadImg(snakeHead);
   document.addEventListener("keydown", snake.logKey.bind(snake));
 }
 
